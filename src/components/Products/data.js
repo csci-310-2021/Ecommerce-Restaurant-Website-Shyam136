3b3654f18523aae1b10a17cd95b3942cccf59bbf
Shyam Patel <patelsb@uindy.edu>
2021-12-08 16:34:32 -0500

    init

---
+++ src/components/Products/data.js
@@ -0,0 +1,66 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+import sweet1 from '../../images/sweet3.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc:
+      'This loaded pizza is the perfect choice for family dinner or a lunch with your crew. Includes pepperoni, seasoned pork, beef, mushrooms, green bell peppers and red onions.',
+    price: '$19.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Pepperoni Pizza',
+    desc:
+      'You literally can not go wrong with pepperoni and mozzarella cheese. Classic for a reason.',
+    price: '$16.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Veggie Overload',
+    desc:
+      'Pizza is officially a vegetable. This garden delight has all the fresh veggie toppings you love: mushrooms, red onions, green bell peppers, Roma tomatoes and black olives.',
+    price: '$14.99',
+    button: 'Add to Cart'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    alt: 'Donuts',
+    name: 'Doughlicious',
+    desc:
+      'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+    price: '$9.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet3,
+    alt: 'Ice Cream',
+    name: 'Caramel Wonder',
+    desc:
+      'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
+    price: '$12.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet1,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: '$9.99',
+    button: 'Add to Cart'
+  }
+];
